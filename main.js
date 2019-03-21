0477cc80eb4947500e9d63173ef28e43c68278fb
mgrinko <glukym@gmail.com>
2019-03-21 10:21:54 +0200

    Schema was created

--- main.js
+++ main.js
@@ -3,3 +3,49 @@
 const SUITS = ['♠', '♣', '♦', '♥'];
 const RED_SUITS = ['♦', '♥'];
 const AVAILABLE_SIGNS = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+
+class Game {
+  render() {
+
+  }
+}
+
+class Stack {
+  constructor(cards) {
+    this.state = {
+      cards: cards,
+    }
+  }
+
+  render() {
+
+  }
+
+  canAccept(cards) {
+    return true;
+  }
+
+  addCards(cards) {
+    this.render();
+  }
+
+  removeCards() {
+    this.render()
+  }
+}
+
+class Card {
+  constructor({ suit, sign }) {
+    this.state = {
+      isOpen: false,
+    }
+  }
+
+  render() {
+
+  }
+
+  toggle(isOpen) {
+    this.state
+  }
+}
